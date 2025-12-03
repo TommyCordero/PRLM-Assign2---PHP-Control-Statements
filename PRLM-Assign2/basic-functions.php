1049cf1bcba5bb148f1e1c17766d25662d812577
Tommy Cordero <tccordero@student.hau.edu.ph>
2025-12-03 23:35:39 +0800

    Create basic-functions.php

---
+++ PRLM-Assign2/basic-functions.php
@@ -0,0 +1,15 @@
+<?php 
+function write_logo(){
+    echo '<img id ="logo" src ="img/logo.png" alt ="logo">';
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>The Doughlicious Store</title>
+        <link rel="stylesheet" href = "css/styles.css"/>
+    </head>
+</html>
+ 
